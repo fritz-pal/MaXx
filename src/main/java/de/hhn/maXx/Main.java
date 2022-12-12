eaf1c585df47e0eb8cd95542fc461a5d1f2acf33
HenriSta <henri@staudenrausch.de>
2022-12-12 17:05:03 +0100

    config file

--- src/main/java/de/hhn/maXx/Main.java
+++ src/main/java/de/hhn/maXx/Main.java
@@ -1,8 +1,8 @@
 package de.hhn.maXx;
 
 public class Main {
+    Object[][] grid = new Object[8][8];
     public static void main(String[] args) {
         System.out.println("Hello World");
-        //Test
     }
 }
