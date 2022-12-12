54e6c943761de90363c8197124a216efc32c2884
2Nadine <nadine.schoch@arcor.de>
2022-12-12 21:27:54 +0100

    Hallo

--- src/main/java/de/hhn/maXx/Main.java
+++ src/main/java/de/hhn/maXx/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Henri");
+        System.out.println("Hallo");
     }
 }
