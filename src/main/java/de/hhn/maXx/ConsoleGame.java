fb423c4e9bf2e0005dadaa125c98b171a3cbd015
HenriSta <henri@staudenrausch.de>
2022-12-19 15:01:43 +0100

    ConsoleGame verbessert

--- src/main/java/de/hhn/maXx/ConsoleGame.java
+++ src/main/java/de/hhn/maXx/ConsoleGame.java
@@ -5,8 +5,10 @@ import de.hhn.maXx.util.Fraction;
 
 public class ConsoleGame {
     Board board;
-    public ConsoleGame(Board board){
+
+    public ConsoleGame(Board board) {
         this.board = board;
+        paint();
     }
 
     public void update(int x, int y, Fraction fraction, FieldState state) {
@@ -14,30 +16,48 @@ public class ConsoleGame {
     }
 
     public void paint() {
-        System.out.println("┌" + "─".repeat(40) + "┐");
+        System.out.println("\n".repeat(100));
+        System.out.println("┌" + "─".repeat(72) + "┐");
+        String emptyLine = "│" + " ".repeat(72) + "│";
 
         for (int y = 0; y < 8; y++) {
             String line1 = "│";
             String line2 = "│";
             String line3 = "│";
-            String emptyLine = "│" + "─".repeat(40) + "│";
             for (int x = 0; x < 8; x++) {
-                if (board.getFieldState(x, y).equals(FieldState.FRACTION)) {
-                    Fraction fraction =  board.getFraction(x, y);
-                    int spacesNum = 3 - fraction.getNumerator().toString().length();
-                    int spacesDen = 3 - fraction.getDenominator().toString().length();
-                    line1 += " " + fraction.getNumerator() + " ";
-                    line2 += " ─── ";
-                    line3 += " " + fraction.getNumerator() + " ";
+                switch (board.getFieldState(x, y)) {
+                    case FRACTION -> {
+                        Fraction fraction = board.getFraction(x, y);
+                        int spacesNum = 3 - fraction.getNumerator().toString().length();
+                        int spacesDen = 3 - fraction.getDenominator().toString().length();
+                        line1 += "   " + " ".repeat(spacesNum) + fraction.getNumerator() + "   ";
+                        line2 += "  ─────  ";
+                        line3 += "   " + " ".repeat(spacesDen) + fraction.getDenominator() + "   ";
+                    }
+                    case EMPTY -> {
+                        line1 += "         ";
+                        line2 += "         ";
+                        line3 += "         ";
+                    }
+                    case WHITE -> {
+                        line1 += "         ";
+                        line2 += "    W    ";
+                        line3 += "         ";
+                    }
+                    case BLACK -> {
+                        line1 += "         ";
+                        line2 += "    B    ";
+                        line3 += "         ";
+                    }
                 }
             }
             line1 += "│";
             line2 += "│";
             line3 += "│";
 
-            System.out.println(emptyLine + line1 + "\n" + line2 + "\n" + line3 + emptyLine);
+            System.out.println(emptyLine + "\n" + line1 + "\n" + line2 + "\n" + line3);
         }
-
-        System.out.println("└" + "─".repeat(40) + "┘");
+        System.out.println(emptyLine);
+        System.out.println("└" + "─".repeat(72) + "┘");
     }
 }
