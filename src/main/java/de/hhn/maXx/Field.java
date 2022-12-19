5cb64c54f40f08f13c2e2f9847fcf9bcad3ee38d
2Nadine <nadine.schoch@arcor.de>
2022-12-19 14:42:35 +0100

    Defaultkonstructor hinzugefügt

--- src/main/java/de/hhn/maXx/Field.java
+++ src/main/java/de/hhn/maXx/Field.java
@@ -1,12 +1,14 @@
 package de.hhn.maXx;
 
 /*
-*
+ *
  */
 
 import de.hhn.maXx.util.Fraction;
 
 import de.hhn.maXx.util.FieldState;
+
+import java.math.BigInteger;
 
 public class Field {
 
@@ -14,16 +16,32 @@ public class Field {
     Fraction fraction;
 
     // Konstruktoren
-    public Field (Fraction fraction, FieldState state){
+    public Field(Fraction fraction, FieldState state) {
         this.fraction = fraction;
         this.state = state;
+    }
+
+    public Field() {
+        state = FieldState.FRACTION;
+        initFraction();
+    }
+
+    private void initFraction() {
+        int randNum = (int) (Math.random() * 1000);
+        int randDen = (int) (Math.random() * 1000);
+        fraction = new Fraction(randNum, randDen);
+        if (fraction.getNumerator().compareTo(new BigInteger("9")) <= 0 || fraction.getNumerator().compareTo(new BigInteger("999")) > 0) {
+            initFraction();
+        }else if(fraction.getDenominator().compareTo(new BigInteger("9")) <= 0 || fraction.getDenominator().compareTo(new BigInteger("999")) > 0){
+            initFraction();
+        }
     }
 
     public void setState(FieldState state) {
         this.state = state;
     }
 
-    public Fraction getFraction(){
+    public Fraction getFraction() {
         return fraction;
     }
 
