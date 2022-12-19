9ae37238b7514d0eb3ce06631a13896585661fd3
2Nadine <nadine.schoch@arcor.de>
2022-12-19 13:27:53 +0100

    Field, neue Methode getFraction()

--- src/main/java/de/hhn/maXx/Field.java
+++ src/main/java/de/hhn/maXx/Field.java
@@ -1,5 +1,17 @@
 package de.hhn.maXx;
 
+/* Was steht in dem Feld
+* - Brüche
+* - Figuren
+ */
+
+import de.hhn.maXx.maXxUtils.Fraction;
+
 public class Field {
+
+
+    public Fraction getFraction(){
+
+    }
 
 }
