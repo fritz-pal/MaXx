557b79a2673a36f04ccfe14cb337db74647a1fa3
HenriSta <henri@staudenrausch.de>
2022-12-19 21:21:17 +0100

    added win sound

--- src/main/java/de/hhn/maXx/Main.java
+++ src/main/java/de/hhn/maXx/Main.java
@@ -3,6 +3,10 @@ package de.hhn.maXx;
 import de.hhn.maXx.util.GameStatus;
 import de.hhn.maXx.util.MyIO;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Schreiben Sie ein (textbasiertes) Anwendungsprogramm, welches es Benutzern erlaubt, das 2-Personen Spiel
  * „MaXx“ auf Spielbrettern der Größe 8 × 8 zu spielen. 62 der Spiel-Felder werden bei Spielbeginn mit zufällig
@@ -39,10 +43,12 @@ public class Main {
                     case WHITE_WIN -> System.out.println("Weiß gewinnt!!");
                     case BLACK_WIN -> System.out.println("Schwarz gewinnt!!");
                 }
+                sound();
             }
             String input = MyIO.promptAndRead("Schreibe PLAY um zu Spielen oder STOP um das Programm zu Beenden: ");
             switch (input.toLowerCase()) {
-                case "play" -> {}
+                case "play" -> {
+                }
                 case "stop" -> {
                     break programmschleife;
                 }
@@ -55,6 +61,18 @@ public class Main {
                 status = Game.getInstance().tick();
             } while (status == GameStatus.CONTINUE);
 
+        }
+    }
+
+    private static void sound() {
+        try {
+            File f = new File("src/main/resources/tadaa.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(f.toURI().toURL());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+            e.printStackTrace();
         }
     }
 }
