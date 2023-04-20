9cb6fbb06d99654be42b4fea9b7883b8d0951c95
fritz-pal <henri@staudenrausch.de>
2023-04-20 17:00:48 +0200

    added indicator to whos turn it is

--- src/main/java/de/hhn/maXx/frontend/MaXxButton.java
+++ src/main/java/de/hhn/maXx/frontend/MaXxButton.java
@@ -1,13 +1,16 @@
 package de.hhn.maXx.frontend;
 
 import de.hhn.maXx.game.Field;
+import de.hhn.maXx.game.Game;
 import de.hhn.maXx.util.FieldState;
 import de.hhn.maXx.util.Fraction;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
 
 public class MaXxButton extends JButton {
     public static final int BUTTON_SIZE = 800 / 8;
@@ -15,10 +18,12 @@ public class MaXxButton extends JButton {
     private final int y;
     private final JLabel nom;
     private final JLabel den;
+    private final Game game;
     boolean isDark;
     private FieldState state = FieldState.FRACTION;
 
-    public MaXxButton(JPanel panel, int x, int y) {
+    public MaXxButton(Game game, JPanel panel, int x, int y) {
+        this.game = game;
         this.x = x;
         this.y = y;
         isDark = (x + y) % 2 == 0;
@@ -32,11 +37,11 @@ public class MaXxButton extends JButton {
         this.addMouseListener(mouseListener());
 
         //nominator label
-        nom = makeNumberLabel("", false);
+        nom = makeNumberLabel(false);
         this.add(nom);
 
         //denominator label
-        den = makeNumberLabel("", true);
+        den = makeNumberLabel(true);
         this.add(den);
 
         this.setBackground((isDark ? new Color(0x2F, 0x31, 0x36) : new Color(0x36, 0x39, 0x3F)));
@@ -45,8 +50,13 @@ public class MaXxButton extends JButton {
         panel.add(this);
     }
 
+    private void drawGradientCircle(Graphics2D g2d) {
+        Color[] colors = {Color.RED, (isDark ? new Color(0x2F, 0x31, 0x36) : new Color(0x36, 0x39, 0x3F))};
+        RadialGradientPaint rgp = new RadialGradientPaint(new Point(50, 50), (float) BUTTON_SIZE / 2, new float[]{0.0f, 0.8f}, colors);
+        g2d.setPaint(rgp);
+        g2d.fill(new Ellipse2D.Double(5, 5, BUTTON_SIZE - 10, BUTTON_SIZE - 10));
+    }
 
-    //called when the window is resized
     @Override
     public void paint(Graphics g) {
         super.paint(g);
@@ -54,28 +64,33 @@ public class MaXxButton extends JButton {
         if (state == FieldState.FRACTION) {
             //draw a horizontal line
             g.setColor(new Color(0x96, 0x98, 0x9D));
+            ((Graphics2D) g).setStroke(new BasicStroke(3));
             g.drawLine(BUTTON_SIZE / 5, BUTTON_SIZE / 2, BUTTON_SIZE / 5 * 4, BUTTON_SIZE / 2);
-            g.drawLine(BUTTON_SIZE / 5, BUTTON_SIZE / 2 + 1, BUTTON_SIZE / 5 * 4, BUTTON_SIZE / 2 + 1);
-            g.drawLine(BUTTON_SIZE / 5, BUTTON_SIZE / 2 - 1, BUTTON_SIZE / 5 * 4, BUTTON_SIZE / 2 - 1);
             nom.setVisible(true);
             den.setVisible(true);
         } else {
             nom.setVisible(false);
             den.setVisible(false);
         }
-        if(state == FieldState.WHITE){
+        if (state == FieldState.WHITE) {
+            if (game.isWhitesTurn()) {
+                drawGradientCircle((Graphics2D) g);
+            }
             g.setColor(Color.WHITE);
-            g.fillOval(BUTTON_SIZE / 4, BUTTON_SIZE / 4, BUTTON_SIZE / 2, BUTTON_SIZE / 2);
+            g.fillOval(BUTTON_SIZE / 3, BUTTON_SIZE / 3, BUTTON_SIZE / 3, BUTTON_SIZE / 3);
         }
-        if(state == FieldState.BLACK){
+        if (state == FieldState.BLACK) {
+            if (!game.isWhitesTurn()) {
+                drawGradientCircle((Graphics2D) g);
+            }
             g.setColor(Color.BLACK);
-            g.fillOval(BUTTON_SIZE / 4, BUTTON_SIZE / 4, BUTTON_SIZE / 2, BUTTON_SIZE / 2);
+            g.fillOval(BUTTON_SIZE / 3, BUTTON_SIZE / 3, BUTTON_SIZE / 3, BUTTON_SIZE / 3);
         }
     }
 
     // creates a label with the given number formatted as a fraction
-    private JLabel makeNumberLabel(String text, boolean bottom) {
-        JLabel num = new JLabel(text);
+    private JLabel makeNumberLabel(boolean bottom) {
+        JLabel num = new JLabel("");
         num.setBounds(0, bottom ? BUTTON_SIZE / 2 + 1 : 0, BUTTON_SIZE, BUTTON_SIZE / 2);
         num.setForeground(new Color(0x96, 0x98, 0x9D));
         num.setFont(new Font("Jetbrains Mono", Font.PLAIN, BUTTON_SIZE / 4));
@@ -95,19 +110,9 @@ public class MaXxButton extends JButton {
     }
 
     private MouseListener mouseListener() {
-        return new MouseListener() {
+        return new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                //TODO
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                //TODO
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
                 //TODO
             }
 
--- src/main/java/de/hhn/maXx/frontend/MaXxWindow.java
+++ src/main/java/de/hhn/maXx/frontend/MaXxWindow.java
@@ -9,10 +9,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MaXxWindow extends JFrame {
-    MaXxButton[][] field = new MaXxButton[8][8];
-    JPanel fieldPanel = new JPanel();
-    JoyStickButton diagonalButton;
-    JPanel joystickPanel = new JPanel();
+    private final MaXxButton[][] field = new MaXxButton[8][8];
+    private final JPanel fieldPanel = new JPanel();
+    private final JoyStickButton diagonalButton;
+    private final JPanel joystickPanel = new JPanel();
     private final Game game;
 
     public MaXxWindow(Game game) {
@@ -33,7 +33,7 @@ public class MaXxWindow extends JFrame {
         this.add(fieldPanel);
 
         //joystick panel
-        joystickPanel.setBounds(850, 300, 200, 200);
+        joystickPanel.setBounds(850, 500, 200, 200);
         joystickPanel.setLayout(new GridLayout(3, 3));
         joystickPanel.setBackground(new Color(0x29, 0x2B, 0x2F));
         this.add(joystickPanel);
@@ -51,16 +51,10 @@ public class MaXxWindow extends JFrame {
         //field buttons
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                field[j][i] = new MaXxButton(fieldPanel, j, i);
+                field[j][i] = new MaXxButton(game, fieldPanel, j, i);
             }
         }
         this.setVisible(true);
-    }
-
-    //called when the window is resized
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
     }
 
     public void update(Board board) {
