4c35f386b6ff43963778af0e626f09e302083aaa
BLVD3 <00nvogel@gmail.com>
2023-04-20 20:17:34 +0200

    Removed Statemanager

--- src/main/java/de/hhn/maXx/game/Game.java
+++ src/main/java/de/hhn/maXx/game/Game.java
@@ -1,7 +1,6 @@
 package de.hhn.maXx.game;
 
 import de.hhn.maXx.frontend.MaXxWindow;
-import de.hhn.maXx.stateMachine.StateManager;
 import de.hhn.maXx.util.Direction;
 import de.hhn.maXx.util.Fraction;
 import de.hhn.maXx.util.GameStatus;
@@ -16,7 +15,6 @@ import de.hhn.maXx.util.GameStatus;
 public class Game {
     private final MaXxWindow window;
     private final Board board;
-    private final StateManager stateManager;
     private Fraction scoreW;
     private Fraction scoreB;
     private boolean whitesTurn = true;
@@ -26,7 +24,6 @@ public class Game {
         this.board = new Board(this);
         this.scoreW = new Fraction(0, 1);
         this.scoreB = new Fraction(0, 1);
-        this.stateManager = new StateManager(this);
         window.update(board);
     }
 
@@ -44,10 +41,6 @@ public class Game {
         return this.board;
     }
 
-    public StateManager getStateManager() {
-        return this.stateManager;
-    }
-
     public void addScoreBlack(Fraction fraction) {
         this.scoreB = this.scoreB.add(fraction);
 
@@ -55,10 +48,6 @@ public class Game {
 
     public void addScoreWhite(Fraction fraction) {
         this.scoreW = this.scoreW.add(fraction);
-    }
-
-    public GameStatus continueGame() {
-        return this.stateManager.turn();
     }
 
     public void move(Direction direction) {
