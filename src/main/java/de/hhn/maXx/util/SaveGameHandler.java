d141dd8ca5838c852b4983718ff258e85da578ad
BLVDE <00nvogel@gmail.com>
2023-04-27 13:14:01 +0200

    loadGame implemented

--- src/main/java/de/hhn/maXx/util/SaveGameHandler.java
+++ src/main/java/de/hhn/maXx/util/SaveGameHandler.java
@@ -6,17 +6,22 @@ import de.hhn.maXx.game.*;
 import java.io.*;
 
 public class SaveGameHandler {
-    public static void saveGame(String path, Game game) {
-        File file = new File(path);
+    public static boolean saveGame(File file, Game game) {
         try {
             ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file));
             stream.writeObject(game);
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
-    public static Game loadGame(String path) {
-
+    public static Game loadGame(String path) throws IOException, ClassNotFoundException {
+        File file = new File(path);
+        if (!file.exists())
+            throw new IOException("File: " + path + " does not exist");
+        ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file));
+        return (Game) stream.readObject();
     }
 }
