4d9f4101bbbebd08b8dea041a891241f234a0298
BLVDE <00nvogel@gmail.com>
2023-04-27 13:06:32 +0200

    saveGame implemented

--- src/main/java/de/hhn/maXx/util/SaveGameHandler.java
+++ src/main/java/de/hhn/maXx/util/SaveGameHandler.java
@@ -3,12 +3,20 @@ package de.hhn.maXx.util;
 import de.hhn.maXx.frontend.*;
 import de.hhn.maXx.game.*;
 
+import java.io.*;
+
 public class SaveGameHandler {
     public static void saveGame(String path, Game game) {
-
+        File file = new File(path);
+        try {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file));
+            stream.writeObject(game);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void loadGame(String path) {
+    public static Game loadGame(String path) {
 
     }
 }
