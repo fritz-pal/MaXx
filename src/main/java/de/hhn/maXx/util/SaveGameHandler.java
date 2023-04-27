2a1c235ad825fca621d3e152e50b0eec70db0851
BLVDE <00nvogel@gmail.com>
2023-04-27 12:53:51 +0200

    Added SaveGameHandler

---
+++ src/main/java/de/hhn/maXx/util/SaveGameHandler.java
@@ -0,0 +1,14 @@
+package de.hhn.maXx.util;
+
+import de.hhn.maXx.frontend.*;
+import de.hhn.maXx.game.*;
+
+public class SaveGameHandler {
+    public static void saveGame(String path, Game game) {
+
+    }
+
+    public static void loadGame(String path) {
+
+    }
+}
